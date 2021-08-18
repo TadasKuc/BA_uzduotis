777ff7e9d6ea94634b09fc3f76c710803c2068d2
TadasKucinskas <tadas.kucinskas1@gmail.com>
2021-08-18 21:18:51 +0300

    initial commit with migrations

---
+++ database/migrations/2021_08_18_173309_create_phones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('person_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->string('phone', 20)->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('person_id')->references('id')->on('persons');
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
