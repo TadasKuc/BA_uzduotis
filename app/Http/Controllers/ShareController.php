475d37dbd8871da6711081f400fa5b4ade7b08c3
TadasKucinskas <tadas.kucinskas1@gmail.com>
2021-08-25 19:36:50 +0300

    Share contacts controler updates

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -6,29 +6,9 @@ use App\Models\SharedContact;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class ShareController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -40,55 +20,17 @@ class ShareController extends Controller
     {
 
         $sharedContact = new SharedContact();
-        $sharedContact->user_id = Auth::user()->id;
-
-
+        $sharedContact->user_from_id = Auth::user()->id;
         $sharedContact->contact_id = $request->get('contact');
-
-
         $phone = $request->get('user_to_phone');
-
-        $toUSer = User::query()->where('phone', '=', $phone)->pluck('id')->first();
-        $sharedContact->user_to_id = $toUSer;
+        $toUser = User::query()->where('phone', '=', $phone)->pluck('id')->first();
+        $sharedContact->user_to_id = $toUser;
         $sharedContact->save();
 
         return redirect(route('contacts.index'));
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
@@ -98,6 +40,9 @@ class ShareController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $shared_contact = SharedContact::query()->find($id);
+
+        $shared_contact->delete();
+
     }
 }
