335f8f023262338249c0e9845c77da3b7df38f4a
TadasKucinskas <tadas.kucinskas1@gmail.com>
2021-08-26 20:36:51 +0300

    unit tests

--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -2,17 +2,45 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+
+use App\Http\Controllers\Web\HomeController;
+use App\Models\Contact;
+use App\Models\User;
+use Tests\TestCase;
 
 class ContactTest extends TestCase
 {
+
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+
+    public function test_only_logged_users_can_see_all_contacts_list()
     {
-        $this->assertTrue(true);
+
+        $this->get('/contacts')->assertRedirect('/login');
     }
+
+    public function test_authenticaded_users_can_see_the_contacts_list()
+    {
+
+        $this->actingAs(User::factory()->create());
+
+        $this->get('/contacts')->assertOk();
+
+    }
+
+    public function test_create_new_contact()
+    {
+
+        Contact::factory()->count(1)->create([
+            'name' => 'test_name'
+        ]);
+
+        $this->assertDatabaseHas('contacts', ['name' => 'test_name']);
+
+    }
+
 }
